db482559d64971d526014765babbfa4586c2c168
Dennis Bogdan Aleksandrov <extraordinarycoutesy@gmail.com>
2021-02-11 21:47:07 -0800

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import LandingPage from './pages/LandingPage';
 import { Provider } from 'react-redux'
 import configureStore from './store';
 import './styles/main.sass';
+import ReactGA from 'react-ga';
 
 function App() {
   const store = configureStore()
+
+  useEffect(() => {
+    ReactGA.initialize('UA-187541969-1');
+    ReactGA.pageview(window.location.pathname + window.location.search + ' portfolio');
+  }, [])
 
   return (
     <div className='backgroundImg'>
