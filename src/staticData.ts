2c251388a6b9d80f4b1fe1e8c50a7925986e9aa9
Dennis Bogdan Aleksandrov <extraordinarycoutesy@gmail.com>
2021-02-11 21:23:29 -0800

    changed out projects to showcase

--- src/staticData.ts
+++ src/staticData.ts
@@ -1,5 +1,5 @@
 
-export const projectsToShowcase = ['Emoji Tack Toes', 'Pao System', "Cinematographer's portfolio"]
+export const projectsToShowcase = ['Movie App', 'Pao System', "Cinematographer's portfolio"]
 
 const customAnimConfig = { mass: 1, tension: 30, friction: 10 }
 const slowAnimConfig = { mass: 1, tension: 56, friction: 50 }
